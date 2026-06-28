092ebbe84a5c57fdac2d2db739ab1a41610f3f91
LCSF2527 <c0964367@mylambton.ca>
2026-06-28 06:52:32 -0400

    styled home page

--- Sidebar_Modal/src/Home.jsx
+++ Sidebar_Modal/src/Home.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { useGlobalContext } from './context'
+import { FaBars } from 'react-icons/fa'
 
 const Home = () => {
-  return <div>Home</div>
+  const { openSidebar, openModal } = useGlobalContext()
+
+  return (
+    <main>
+      <button onClick={openSidebar} className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button onClick={openModal} className="btn">
+        show modal
+      </button>
+    </main>
+  )
 }
 
 export default Home
